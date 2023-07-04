5b5a8aa26c7424b4ef45a317853df455eb68bac2
CooledxD <scribe_85@mail.ru>
2023-07-05 02:14:09 +0700

    implemented the accessToken update function

--- src/utils/ky/ky.js
+++ src/utils/ky/ky.js
@@ -1,5 +1,8 @@
 import ky from "ky";
 
+import { store } from "../../store/config.js";
+import { set } from "../../store/features/auth/authSlice.js";
+
 export default ky.create({
   prefixUrl: import.meta.env.VITE_HOST,
   hooks: {
@@ -8,6 +11,35 @@ export default ky.create({
         const { message } = await error.response.json()
         error.message = message
         return error
+      }
+    ],
+    beforeRequest: [
+      request => {
+        request.headers.set(
+          'Authorization', 
+          `Bearer ${store.getState().auth.accessToken}`
+        )
+      }
+    ],
+    afterResponse: [
+      async (request, _, response) => {
+        if (response.status === 401) {
+          const {accessToken, refreshToken} = await ky.post(`${import.meta.env.VITE_HOST}/auth/refresh`, {
+            json: {
+              refreshToken: localStorage.getItem('refreshToken')
+            }
+          }).json()
+
+          store.dispatch(set(accessToken))
+          localStorage.setItem('refreshToken', refreshToken)
+
+          request.headers.set(
+            'Authorization', 
+            `Bearer ${accessToken}`
+          )
+
+          return ky(request)
+        }
       }
     ]
   }
